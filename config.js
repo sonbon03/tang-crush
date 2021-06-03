7751f5c6e38e0f3fd775e232452ca17af689231f
sonbon03 <85279633+sonbon03@users.noreply.github.com>
2021-06-03 16:01:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Thuý à!',
+    introTitle: 'Mọi người à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Thuý cứ đi nhầm đường
-    Lạc vào tim Sơn lẻ loi
+    Mà sao Mọi người cứ đi nhầm đường
+    Lạc vào tim Em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '😋😋',
-    title: 'Phải chăng Thuý đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Thuý đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng Mọi người đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Mọi người đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Sơn <33',
     btnNo: 'Không, Bơn à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Bơn lại yêu Sơn <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Mọi người lại yêu Sơn <3',
     btnReply: 'Gửi cho Sơn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Sơn biết mà 🥰. Yêu Thuý nhiều nhiều 😘😘',
-    messDesc: 'Yêu bé Thuý',
+    mess: 'Sơn biết mà 🥰. Yêu Mọi người nhiều nhiều 😘😘',
+    messDesc: 'Yêu fullpeople',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/sonbonbeo2003' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
