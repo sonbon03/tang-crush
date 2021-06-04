2e7ee3d1ddf2d990c9de3430e58cebefaa0bae2f
sonbon03 <85279633+sonbon03@users.noreply.github.com>
2021-06-04 17:49:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 const CONFIG = {
-    introTitle: 'Linh à!',
+    introTitle: 'Chi à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao Linh cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao Chi lại chọn ăn gì cũng được`,
     btnIntro: '😋😋',
-    title: 'Phải chăng Linh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng Linh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích Bơn <33',
-    btnNo: 'Không, Bơn à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao Linh lại yêu Bơn <3',
+    title: 'Phải chăng Chi thì ăn ... 😙',
+    desc: 'Phải chăng Chi... ',
+    btnYes: 'Ăn cứt',
+    btnNo: 'Ăn cơm',
+    btnNo: 'Ăn 
+    question:'Trên thế giới hơn 7 tỉ người mà sao Chi lại thích ăn cứt',
     btnReply: 'Gửi cho Bơn <3',
-    reply: 'Yêu Bơn nhiều <33333333',
-    mess: 'Bơn biết mà 🥰. Yêu Linh nhiều nhiều 😘😘',
-    messDesc: 'Yêu bé Linh',
+    reply: 'Thích thì ăn thôi',
+    mess: 'Lêuleeu',
+    messDesc: 'Hâhhahahahaha',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.youtube.com/watch?v=ZqDBgYPpUTg' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
